dba8cc8a7ee69bc69834a3d3dacd7100fabd6522
dieguitojd <diegoaw591@gmail.com>
2021-07-11 20:40:37 -0500

    Add smooth scroll to about.html, change header text

--- js/adobe.js
+++ js/adobe.js
@@ -1,32 +1,34 @@
 document.addEventListener("adobe_dc_view_sdk.ready", function () {
   var adobeDCView = new AdobeDC.View({
-    clientId: "7b07bacbba93425e80de5cebb06f527b",
+    clientId: "6f8d56f7b3154dcf99f211f51fbf2b79",
     divId: "adobe-dc-view",
   });
   adobeDCView.previewFile(
     {
-      content: { location: { url: "http://www.tu990.mx/pdf/gaceta.pdf" } },
+      content: { location: { url: "https://www.tu990.mx/pdf/gaceta.pdf" } },
       metaData: { fileName: "gaceta.pdf" },
     },
     { embedMode: "SIZED_CONTAINER" }
   );
 });
 
-document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-  var adobeDCView = new AdobeDC.View({clientId: "7b07bacbba93425e80de5cebb06f527b", divId: "adobe-dc-view2"});
-  adobeDCView.previewFile({
-    content:{location: {url: "http://www.tu990.mx/pdf/aviso.pdf"}},
-    metaData:{fileName: "Aviso.pdf"}
-  }, {embedMode: "IN_LINE"});
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+  var adobeDCView = new AdobeDC.View({
+    clientId: "6f8d56f7b3154dcf99f211f51fbf2b79",
+    divId: "adobe-dc-view2",
+  });
+  adobeDCView.previewFile(
+    {
+      content: { location: { url: "https://www.tu990.mx/pdf/aviso.pdf" } },
+      metaData: { fileName: "Aviso.pdf" },
+    },
+    { embedMode: "IN_LINE" }
+  );
 });
-
-
-
-
 
 /* Pass the embed mode option here */
 const viewerConfig = {
-  embedMode: "LIGHT_BOX"
+  embedMode: "LIGHT_BOX",
 };
 
 /* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
@@ -35,22 +37,22 @@ document.addEventListener("adobe_dc_view_sdk.ready", function () {
 });
 
 /* Function to render the file using PDF Embed API. */
-function previewFile()
-{
+function previewFile() {
   /* Initialize the AdobeDC View object */
   var adobeDCView = new AdobeDC.View({
-      /* Pass your registered client id */
-      clientId: "7b07bacbba93425e80de5cebb06f527b"
+    /* Pass your registered client id */
+    clientId: "6f8d56f7b3154dcf99f211f51fbf2b79",
   });
 
   /* Invoke the file preview API on Adobe DC View object */
-  adobeDCView.previewFile({
+  adobeDCView.previewFile(
+    {
       /* Pass information on how to access the file */
       content: {
-          /* Location of file where it is hosted */
-          location: {
-              url: "http://www.tu990.mx/pdf/vehiculodc.pdf",
-              /*
+        /* Location of file where it is hosted */
+        location: {
+          url: "https://www.tu990.mx/pdf/vehiculodc.pdf",
+          /*
               If the file URL requires some additional headers, then it can be passed as follows:-
               header: [
                   {
@@ -59,32 +61,34 @@ function previewFile()
                   }
               ]
               */
-          },
+        },
       },
       /* Pass meta data of file */
       metaData: {
-          /* file name */
-          fileName: "vehiculodoblecero.pdf"
-      }
-  }, viewerConfig);
-};
+        /* file name */
+        fileName: "vehiculodoblecero.pdf",
+      },
+    },
+    viewerConfig
+  );
+}
 
-function previewFile2()
-{
+function previewFile2() {
   /* Initialize the AdobeDC View object */
   var adobeDCView = new AdobeDC.View({
-      /* Pass your registered client id */
-      clientId: "7b07bacbba93425e80de5cebb06f527b"
+    /* Pass your registered client id */
+    clientId: "6f8d56f7b3154dcf99f211f51fbf2b79",
   });
 
   /* Invoke the file preview API on Adobe DC View object */
-  adobeDCView.previewFile({
+  adobeDCView.previewFile(
+    {
       /* Pass information on how to access the file */
       content: {
-          /* Location of file where it is hosted */
-          location: {
-              url: "http://www.tu990.mx/pdf/vehiculoexento.pdf",
-              /*
+        /* Location of file where it is hosted */
+        location: {
+          url: "https://www.tu990.mx/pdf/vehiculoexento.pdf",
+          /*
               If the file URL requires some additional headers, then it can be passed as follows:-
               header: [
                   {
@@ -93,12 +97,14 @@ function previewFile2()
                   }
               ]
               */
-          },
+        },
       },
       /* Pass meta data of file */
       metaData: {
-          /* file name */
-          fileName: "vehiculoexento.pdf"
-      }
-  }, viewerConfig);
-};
+        /* file name */
+        fileName: "vehiculoexento.pdf",
+      },
+    },
+    viewerConfig
+  );
+}
--- js/mapa.js
+++ js/mapa.js
@@ -1,9 +1,8 @@
-let donnies;
 let map;
 function initMap() {
-  // la ubicacion de donnies
-  ubicacion = { lat: 19.608990887875315, lng: -99.18054933962361 };
-  // El mapa centrado en donnies
+  
+  let ubicacion = { lat: 19.608990887875315, lng: -99.18054933962361 };
+  
   
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
@@ -33,11 +32,13 @@ function initMap() {
           "<a href='https://www.google.com/maps/dir//Verificentro+TU-990,+Blvrd+Reforma,+Fimesa+I,+54946+Buenavista,+M%C3%A9x./@19.6086702,-99.1825924,17z/data=!4m16!1m6!3m5!1s0x85d1f605112d5e69:0xeba8dda288da2429!2sVerificentro+TU-990!8m2!3d19.6089063!4d-99.1805455!4m8!1m0!1m5!1m1!1s0x85d1f605112d5e69:0xeba8dda288da2429!2m2!1d-99.1805455!2d19.6089063!3e2' target='_blank'>Indicaciones para llegar</a>" +
           "</div>"
       );
-      infowindow.open(map, marker);
+      infowindow.open({
+        anchor: marker,
+        map, 
+        shouldFocus: false
+      });
     }
   });
 }
 
-// El marcardor, posicionado en el centro de donnies
-/*marker = new google.maps.Marker({position: donnies, map: map, title: 'Donnies guitars'});
-    marker.setAnimation(google.maps.Animation.BOUNCE);*/
+
